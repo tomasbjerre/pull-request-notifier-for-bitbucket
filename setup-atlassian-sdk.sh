3f63db715413e9b2d6ccf1c43912f8ea41cfeedb
Tomas Bjerre <tomas.bjerre85@gmail.com>
2017-05-13 11:01:49 +0200

    Updating install script

--- setup-atlassian-sdk.sh
+++ setup-atlassian-sdk.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-installdir=`pwd`
+
+if [ "$1" -gt "-1" ]
+  then echo Give param, like /home/username
+fi
+
+installdir=$1
+cd $1
 wget https://marketplace.atlassian.com/download/plugins/atlassian-plugin-sdk-tgz
 mkdir opt
 tar -xvzf *plugin-sdk* -C opt
